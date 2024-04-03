ecfad29ea7fbeb12c9b6987b0809c2d396f1228a
micheal-donald <michaelmonyenye@gmail.com>
2024-04-03 22:44:58 +0300

    created  directory named public at the root to place static files

--- HTMLemailtemplate/server.js
+++ HTMLemailtemplate/server.js
@@ -1,10 +1,23 @@
-//intialise an express server
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
-//require handlebars
-const handlebars = require('express-handlebars');
+// Require express-handlebars
+const { engine } = require('express-handlebars');
 
+// Set up the handlebars engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+// Serve static files
+app.use(express.static('public'));
+ 
+// Define a route for the root of the site
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Start the server
 app.listen(8080, () => {
-  console.log('server started', 8080);
+  console.log('Server started on port', 8080);
 });
